5f38c2d357449501808a381ff6ad6a4ed888b051
Dmitry Shapkin <shapkindev@gmail.com>
2019-03-11 22:26:21 +0300

    Added Lesson-21 UIView Animations

---
+++ SK-Lesson-21 UIView Animations/SK-Lesson-21 UIView Animations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SK-Lesson-21 UIView Animations
+//
+//  Created by Dmitry Shapkin on 11/03/2019.
+//  Copyright © 2019 Dmitry Shapkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
