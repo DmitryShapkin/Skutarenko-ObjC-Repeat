ca9092ee145994b48932873bd09ec06f02a7b0ad
Dmitry Shapkin <shapkindev@gmail.com>
2019-03-11 14:35:51 +0300

    Added Lesson-18 Thinking out of the box (empty)

---
+++ SK-Lesson-18 Thinking out of the box/SK-Lesson-18 Thinking out of the box/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SK-Lesson-18 Thinking out of the box
+//
+//  Created by Dmitry Shapkin on 11/03/2019.
+//  Copyright © 2019 Dmitry Shapkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SK-Lesson-18 Thinking out of the box/SK-Lesson-18 Thinking out of the box/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SK-Lesson-18 Thinking out of the box
+//
+//  Created by Dmitry Shapkin on 11/03/2019.
+//  Copyright © 2019 Dmitry Shapkin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
