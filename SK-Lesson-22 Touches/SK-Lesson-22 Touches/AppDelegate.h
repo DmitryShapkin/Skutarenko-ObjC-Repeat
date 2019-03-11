d5ecc0e76972205d8c300badbfebfbe0f5f7454f
Dmitry Shapkin <shapkindev@gmail.com>
2019-03-12 01:15:58 +0300

    Added Lesson-22 Touches

---
+++ SK-Lesson-22 Touches/SK-Lesson-22 Touches/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SK-Lesson-22 Touches
+//
+//  Created by Dmitry Shapkin on 11/03/2019.
+//  Copyright © 2019 Dmitry Shapkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SK-Lesson-22 Touches/SK-Lesson-22 Touches/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SK-Lesson-22 Touches
+//
+//  Created by Dmitry Shapkin on 11/03/2019.
+//  Copyright © 2019 Dmitry Shapkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
